5e639c94f1693ce37576980f1202f6389770c7b9
todjiaa <todjiaa@todjiaa.com>
2021-08-10 13:20:27 +0300

    Added  resize event listener

--- main.js
+++ main.js
@@ -6,40 +6,94 @@ const urlInput = document.querySelector(".url-input");
 
 const startStreamButton = document.querySelector(".start-stream-button");
 
-const playerWidth = 600;
-const playerHeight = 339;
+let playerWidth = 600;
+let playerHeight = 339;
 
-playerPlaceholder.style.width = playerWidth + "px";
-playerPlaceholder.style.height = playerHeight + "px";
-
-const startStream = (e, urlInputValue) => {
-    const player = e.target.parentNode.previousElementSibling.firstElementChild;
-    if (player) {
-        restartPlayer(player);
-    }
-    
-    playerInit(urlInputValue);
+// Set the widht and height of the video player placeholder
+const setWidthHeight = (element, width, height) => {
+    element.style.width = width + "px";
+    element.style.height = height + "px";
 }
-startStreamButton.addEventListener("click", (e) => {
-    startStream(e, urlInput.value)
-});
 
-const playerInit = (urlInputValue) => {
+// Set the size on Window load
+const onWindowLoad = () => {
+    playerSize();
+}
+window.addEventListener("load", onWindowLoad);
+
+// Set the size on window resize
+const onWindowResize = () => {
+    if (window.innerWidth < playerWidth) {
+        playerSize();
+
+        restartPlayer();
+
+        playerInit();
+    }
+}
+window.addEventListener("resize", onWindowResize);
+
+// Get and set the width and height for the player and the player placeholder dynamically depending on the size of the screen
+const playerSize = () => {
+    let width = playerWidth;
+    let height = playerHeight;
+
+    const ratio = playerWidth/playerHeight;
+
+    if (window.innerWidth < playerWidth) {
+        width = window.innerWidth;
+        height = width / ratio;
+
+        setWidthHeight(playerPlaceholder, width, height);
+    }
+    else {
+        width = playerWidth;
+        height = playerHeight;
+
+        setWidthHeight(playerPlaceholder, width, height);
+    }
+
+    return {
+        width: width,
+        height: height
+    }
+}
+
+// Start the stream
+const startStream = (e) => {
+    restartPlayer();
+    
+    playerInit();
+}
+startStreamButton.addEventListener("click", startStream);
+
+// Restart the video player
+const restartPlayer = () => {
+    const player = document.querySelector(".sldp_player_wrp");
+
+    if (player && player.classList.contains("sldp_player_wrp")) {
+        player.remove();
+    }
+}
+
+// Initialize the video player
+const playerInit = () => {
+    console.log(playerSize().width)
+
+    const activeWidth = playerSize().width;
+    const activeHeight = playerSize().height;
+
+    setWidthHeight(playerPlaceholder, activeWidth, activeHeight);
+
     SLDP.init({
         container:          `player-placeholder`,
-        stream_url:         urlInputValue,
+        stream_url:         urlInput.value,
         initial_resolution: '240p',
         buffering:          500,
         autoplay:           true,
         muted:              false,
-        width:              playerWidth,
-        height:             playerHeight,
-        fullscreen:         true
+        width:              activeWidth,
+        height:             activeHeight,
+        // fullscreen:         true
     })
-}
-
-const restartPlayer = (player) => {
-    if (player.classList.contains("sldp_player_wrp")) {
-        player.remove();
-    }
 }
